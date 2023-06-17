ebf72a11fec14eb4fa82d662d748c8d5e8005556
Kyriel Abad <abadkyriel@gmail.com>
2023-06-17 23:27:38 +0800

    Create skeleton files for Manager

--- src/dmanager/dmanager.ts
+++ src/dmanager/dmanager.ts
@@ -1,1 +1,17 @@
-export class DManager {}
+import { DDatabase } from "../database";
+
+export class DManager {
+  private database: DDatabase;
+  public constructor(database: DDatabase) {
+    this.database = database;
+  }
+
+  public resolve() {}
+
+  public update() {}
+
+  public ballot() {}
+
+  public book() {}
+}
+
