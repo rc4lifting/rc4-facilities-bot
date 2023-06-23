e68d45bdaf4a75187783a9695e7c7132daa9cf35
Kyriel Abad <abadkyriel@gmail.com>
2023-06-23 19:04:09 +0800

    Resolve error in calculation of days in DManager

--- src/dmanager/dmanager.ts
+++ src/dmanager/dmanager.ts
@@ -1,22 +1,37 @@
+import { start } from "repl";
 import { DDatabase, Ballot } from "../database";
 import { shuffle } from "./algorithms";
-import { previousSunday } from "date-fns";
+import { previousSunday, addDays } from "date-fns";
 
 export class DManager {
   private database: DDatabase;
   public constructor(database: DDatabase) {
     this.database = database;
   }
+  
+  /**
+   * Using the current time, get the start of what we define 
+   * as the "previous week" - Monday to Sunday.
+   * 
+   * @returns The start of the current week, Monday.
+   */
+  private weekStart(): Date {
+    const start = previousSunday(Date.now())
+    start.setHours(0);
+    start.setMinutes(0)
+    start.setSeconds(0)
+    start.setMilliseconds(0)
+    return addDays(start, 1);
+  }
 
   /**
    * Resolves the next week of ballots.
    */
   public async resolve(): Promise<void> {
-    const weekStart = previousSunday(Date.now());
     // Start of balloting period - next week
-    const ballotStart = new Date(weekStart.getDate() + 7);
+    const ballotStart = addDays(this.weekStart(), 7)
     // End of balloting period - next next week
-    const ballotEnd = new Date(ballotStart.getDate() + 7);
+    const ballotEnd = addDays(ballotStart, 7);
     const ballots = await this.database.getBallotsByTime(
       ballotStart,
       ballotEnd
@@ -32,8 +47,13 @@ export class DManager {
           startTime: shuffled[x].time_begin,
           endTime: shuffled[x].time_end,
         });
-      } catch (e) {
+      } catch (error) {
         // If there's an error from conflicting bookings, no issue!
+        if (error instanceof Error && error.message == "Unable to book the entire slot, part/all of it is already booked") {
+          continue
+        } else {
+          throw error;
+        }
       }
     }
   }
@@ -47,10 +67,9 @@ export class DManager {
     startTime: Date,
     endTime: Date
   ): Promise<void> {
-    const weekStart = previousSunday(Date.now());
     if (
-      startTime < new Date(weekStart.getDate() + 7) ||
-      startTime >= new Date(weekStart.getDate() + 14)
+      startTime < addDays(this.weekStart(), 7) ||
+      startTime >= addDays(this.weekStart(), 14)
     ) {
       throw new Error("Balloting can only be done for the next week!");
     }
@@ -66,9 +85,8 @@ export class DManager {
     startTime: string;
     endTime: string;
   }): Promise<void> {
-    const weekStart = previousSunday(Date.now());
     const startTime = new Date(booking.startTime);
-    if (startTime >= new Date(weekStart.getDate() + 7)) {
+    if (startTime >= addDays(this.weekStart(), 7) || startTime < this.weekStart()) {
       throw new Error("Booking can only be done for the current week!");
     }
     const res = await this.database.bookSlot(booking);
