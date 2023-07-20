3157736e4f075367fd5d56d2445a13b98fca5567
Kyriel Abad <abadkyriel@gmail.com>
2023-07-21 01:44:32 +0800

    Create skeleton d-visualiser

---
+++ src/dvisualiser/dvisualiser.ts
@@ -0,0 +1,13 @@
+import { Ballot, Slot } from "../database";
+import Plotly from "plotly.js";
+
+/**
+ * A class that generates a visualisation of ballots or slots
+ */
+export class DVisualiser {
+  /**
+   * Given an allocation of ballots, generate a heatmap of 
+   */
+  static showBallots() {}
+  static showSlots() {}
+}
